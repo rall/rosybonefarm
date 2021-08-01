2f464c376ef799c16256f298f226d47fcacf19f4
Richard Allaway <richard.allaway@gmail.com>
2021-08-01 13:46:37 -0400

    import inventory controller and service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { InventoryController } from '../inventory/inventory.controller';
+import { InventoryService } from '../inventory/inventory.service';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, InventoryController],
+  providers: [AppService, InventoryService],
 })
 export class AppModule {}
