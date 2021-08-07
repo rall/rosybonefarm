64ddfe80aaf691e7a8d81df438447cb49ce0e8cd
Richard Allaway <richard.allaway@gmail.com>
2021-08-07 15:57:16 -0400

    remove cruft

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { InventoryController } from '../inventory/inventory.controller';
-import { InventoryService } from '../inventory/inventory.service';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, InventoryController],
-  providers: [AppService, InventoryService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
