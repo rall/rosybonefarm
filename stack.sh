2e57d93edf37b18bca6eab426ca855e9e89ef34c
Richard Allaway <richard.allaway@gmail.com>
2021-08-07 15:54:57 -0400

    switch to postgres

--- stack.sh
+++ stack.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker image build --tag="rosybone/api" docker/api/
-docker image build --tag="rosybone/sqlite3" docker/sqlite3/
 docker stack deploy --compose-file docker-compose.yaml rosybone
