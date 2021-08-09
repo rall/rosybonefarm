6ffb850522c84b80c648c4130f67866cce654c90
Richard Allaway <richard.allaway@gmail.com>
2021-08-09 09:04:06 -0400

    build postgres image in build

--- stack.sh
+++ stack.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker image build --tag="rosybone/api" docker/api/
+docker image build --tag="rosybone/postgres" docker/postgres/
 docker stack deploy --compose-file docker-compose.yaml rosybone
