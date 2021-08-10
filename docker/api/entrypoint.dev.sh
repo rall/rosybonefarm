45dc79e7b0cbfcf9c81fc80b1eb43a90a8961f5b
Richard Allaway <richard.allaway@gmail.com>
2021-08-10 11:34:39 -0400

    add todo

--- docker/api/entrypoint.dev.sh
+++ docker/api/entrypoint.dev.sh
@@ -3,6 +3,7 @@ export DATABASE_URL=postgresql://${POSTGRES_APP_USER}:${POSTGRES_APP_PASSWORD}@$
 cd nestjs
 PRISMA_CLI_BINARY_TARGET=linux-arm-openssl-1.1.x npm install
 ln -s ../prisma/ ./db
+# TODO why can't I run `prisma migrate dev` here?
 npx prisma generate --schema=db/schema.prisma
 echo "starting api"
 nodemon main.js
