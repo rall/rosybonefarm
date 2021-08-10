ee496f411249d8228a4ee063381256a282c1aacd
Richard Allaway <richard.allaway@gmail.com>
2021-08-10 13:53:47 -0400

    check for symlink before creating

--- docker/api/entrypoint.dev.sh
+++ docker/api/entrypoint.dev.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 export DATABASE_URL=postgresql://${POSTGRES_APP_USER}:${POSTGRES_APP_PASSWORD}@${DATABASE_HOST}:5432/${POSTGRES_APP_DB}
 cd nestjs
-ln -s ../prisma/ ./db
+
+if [ ! -L db ]
+then
+  ln -s ../prisma db
+fi
 
 npm install
+
 # TODO why can't I run `prisma migrate dev` here?
 npx prisma generate --schema=db/schema.prisma
+
 echo "starting api"
 nodemon main.js
