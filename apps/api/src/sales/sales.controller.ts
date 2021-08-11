fbfd0b46c4797e61ac7a46ab077fc34f80de95be
Richard Allaway <richard.allaway@gmail.com>
2021-08-11 15:05:29 -0400

    handle db constraint exception (sale has already been made in this case)

--- apps/api/src/sales/sales.controller.ts
+++ apps/api/src/sales/sales.controller.ts
@@ -1,5 +1,5 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
-import { EggsDozen, Sale } from '@prisma/client';
+import { BadRequestException, Body, Controller, Get, HttpException, HttpStatus, Logger, Post } from '@nestjs/common';
+import { EggsDozen, Prisma, Sale } from '@prisma/client';
 import { SalesService } from '../sales.service';
 
 @Controller('sales')
@@ -10,8 +10,15 @@ export class SalesController {
   async create(@Body() postData: { userId: number, eggsDozenId: number }): Promise<EggsDozen> {
     if (!postData.userId || !postData.eggsDozenId) {
       throw new BadRequestException('userId & eggsDozenId both required');
-    } else {
+    }
+    try {
       return await this.salesService.createSale(postData);
+    } catch (error: unknown) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code == "P2014") {
+        throw new HttpException('Forbidden', HttpStatus.GONE);
+      } else {
+        throw error;
+      }
     }
   }
   
