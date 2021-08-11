fa63a467df06db33098836f08f9232e616538f23
Richard Allaway <richard.allaway@gmail.com>
2021-08-11 11:59:22 -0400

    clear stack before rebuilding

--- stack.sh
+++ stack.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker image build --tag="rosybone/api" docker/api/
 docker image build --tag="rosybone/postgres" docker/postgres/
+docker stack rm rosybone
 docker stack deploy --compose-file docker-compose.yaml rosybone
